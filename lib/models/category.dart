2334b8cede49d662f8c31dbddec6be73aca92409
Adam Abah <danielabah45@gmail.com>
2025-08-30 21:22:03 +0100

    Initial Commit

---
+++ lib/models/category.dart
@@ -0,0 +1,45 @@
+class CategoryModel {
+  String vector;
+  bool isSelected;
+  CategoryModel({
+    required this.vector,
+    required this.isSelected,
+  });
+
+  static List<CategoryModel> getCategories() {
+
+    List<CategoryModel> categories = [];
+
+    categories.add(
+      CategoryModel(
+        vector: 'assets/vectors/heart.svg', 
+        isSelected: false
+        )
+    );
+
+    categories.add(
+      CategoryModel(
+        vector: 'assets/vectors/pill.svg', 
+        isSelected: false
+        )
+    );
+
+    categories.add(
+      CategoryModel(
+        vector: 'assets/vectors/tooth.svg', 
+        isSelected: true
+        )
+    );
+
+    categories.add(
+      CategoryModel(
+        vector: 'assets/vectors/pregnant.svg', 
+        isSelected: false
+        )
+    );
+
+    return categories;
+
+  }
+
+}
